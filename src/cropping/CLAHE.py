fd9a0a640b0a48098f587be2029600b00b7563ee
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2019-05-10 19:45:05 +0100

    Update CLAHE.py

--- src/cropping/CLAHE.py
+++ src/cropping/CLAHE.py
@@ -22,8 +22,8 @@ def CLAHE(input_data_folder):
     Exams=[]
     for i in range(0,len(files)):
         #print(files[i], input_data_folder)
-        bgr = cv2.imread(input_data_folder+'/'+files[i])
-        lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+        image = np.array(imageio.imread(file_name))
+        lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
         lab_planes = cv2.split(lab)
         clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(256,256))
         lab_planes[0] = clahe.apply(lab_planes[0])
@@ -34,9 +34,9 @@ def CLAHE(input_data_folder):
         #heatmap = cv2.applyColorMap(bgr, cv2.COLORMAP_HOT)
         heatmap = cv2.applyColorMap(bgr, cv2.COLORMAP_OCEAN)
         heatmap = np.array(heatmap)
-        img = Image.fromarray(heatmap)
+       
         img.save(input_data_folder+'/'+files[i], bits=16)
-
+ 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Apply CLAHE and CMAP')
     parser.add_argument('--input-data-folder', required=True)
