c2e1a6ed8b312ffb529b6b3a494a374cc60a0f3a
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2019-05-10 23:02:18 +0100

    Update CLAHE.py

--- src/cropping/CLAHE.py
+++ src/cropping/CLAHE.py
@@ -73,7 +73,7 @@ def CLAHE(input_data_folder):
         clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(256,256))
         lab_planes[0] = clahe.apply(lab_planes[0])
         lab = cv2.merge(lab_planes)
-        bgr = cv2.cvtColor(lab, cv2.COLOR_BGR2GRAY)
+        bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
         #heatmap = cv2.applyColorMap(bgr, cv2.COLORMAP_HOT)
         #heatmap = cv2.applyColorMap(bgr, cv2.COLORMAP_OCEAN)
         #heatmap = np.array(heatmap)
