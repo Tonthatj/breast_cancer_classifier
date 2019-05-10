867ceeaddec91e2d8d5be6bc640e4adf4743291c
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2019-05-10 20:12:20 +0100

    Update CLAHE.py

--- src/cropping/CLAHE.py
+++ src/cropping/CLAHE.py
@@ -10,6 +10,49 @@ import numpy as np
 import cv2
 import imageio
 
+def save_hdr(filename, img, dimension=None, gray=False):
+    dims = len(img.shape)
+    if not (1 < dims <= 3):
+        eprint('Unsupported number of dimensions')
+        sys.exit(1)
+    if dims == 3:  # volumetric data is stored slice by slice
+        name, ext = os.path.splitext(filename)
+        if dimension is None:
+            # smallest:
+            if min(img.shape) < 20:
+                dimension = np.argmin(img.shape)
+            # if NxNxM -> M
+            elif img.shape[0] == img.shape[1]:
+                dimension = 2
+            # if NxMxM -> N
+            elif img.shape[1] == img.shape[2]:
+                dimension = 0
+            # if nothing works, use default 0
+            else:
+                dimension = 0
+        for i in range(img.shape[dimension]):
+            fname = '{}_{}{}'.format(name, i, ext)
+            if dimension == 0:
+                save_hdr(fname, img[i, ...])
+            if dimension == 1:
+                save_hdr(fname, img[:, i, :])
+            if dimension == 2:
+                save_hdr(fname, img[..., i])
+    else:  # 2D data
+        if not gray:
+            # Transform gray it to RGB
+            arr = np.dstack((img, img, img))
+        else:
+            arr = img
+        if filename.endswith('.png'):
+            imageio.imsave(filename, arr, format='PNG-FI')
+        elif filename.endswith('.tiff'):
+            tiff.imsave(filename, arr)
+        else:
+            eprint('Unsupported file format!')
+            sys.exit(1)
+
+
 def CLAHE(input_data_folder):
     # r=root, d=directories, f = files
     files=[]
@@ -31,11 +74,10 @@ def CLAHE(input_data_folder):
         lab = cv2.merge(lab_planes)
         bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
         bgr = np.array(bgr)
-  
         #heatmap = cv2.applyColorMap(bgr, cv2.COLORMAP_HOT)
-        heatmap = cv2.applyColorMap(bgr, cv2.COLORMAP_OCEAN)
-        heatmap = np.array(heatmap)
-        imageio.imwrite(input_data_folder+'/'+files[i], heatmap)
+        #heatmap = cv2.applyColorMap(bgr, cv2.COLORMAP_OCEAN)
+        #heatmap = np.array(heatmap)
+        save_hdr(input_data_folder+'/'+files[i], bgr.astype(np.uint16), options.dimension, gray=options.gray)
  
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Apply CLAHE and CMAP')
