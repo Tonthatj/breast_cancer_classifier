09f1591f8d8d59b2c55e34848c119c07762cb0bc
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2019-05-10 17:53:14 +0100

    Update CLAHE.py

--- src/cropping/CLAHE.py
+++ src/cropping/CLAHE.py
@@ -19,6 +19,7 @@ def CLAHE(input_data_folder):
 
     Exams=[]
     for i in range(0,len(files)):
+        print(files[i], input_data_folder)
         bgr = cv2.imread(input_data_folder+files[i])
         lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
         lab_planes = cv2.split(lab)
