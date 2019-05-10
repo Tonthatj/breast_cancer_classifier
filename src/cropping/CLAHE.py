5e638a7e96acc5986687b1fb7cd09b2b218dc8f6
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2019-05-10 17:10:24 +0100

    Update CLAHE.py

--- src/cropping/CLAHE.py
+++ src/cropping/CLAHE.py
@@ -1,1 +1,13 @@
 
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Apply CLAHE and CMAP')
+    parser.add_argument('--input-data-folder', required=True)
+    args = parser.parse_args()
+
+   
+    CLAHE( input_data_folder=args.input_data_folder, )
