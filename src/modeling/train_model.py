e457c4611fb9572417190fa6e74e8b715bbd48b3
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2019-05-15 14:10:15 +0100

    Update train_model.py

--- src/modeling/train_model.py
+++ src/modeling/train_model.py
@@ -1,82 +1,19 @@
-# Copyright (C) 2019 Nan Wu, Jason Phang, Jungkyu Park, Yiqiu Shen, Zhe Huang, Masha Zorin, 
-
-#   Stanisław Jastrzębski, Thibault Févry, Joe Katsnelson, Eric Kim, Stacey Wolfson, Ujas Parikh, 
-
-#   Sushma Gaddam, Leng Leng Young Lin, Kara Ho, Joshua D. Weinstein, Beatriu Reig, Yiming Gao, 
-
-#   Hildegard Toth, Kristine Pysarenko, Alana Lewin, Jiyon Lee, Krystal Airola, Eralda Mema, 
-
-#   Stephanie Chung, Esther Hwang, Naziya Samreen, S. Gene Kim, Laura Heacock, Linda Moy, 
-
-#   Kyunghyun Cho, Krzysztof J. Geras
-
-#
-
-# This file is part of breast_cancer_classifier.
-
-#
-
-# breast_cancer_classifier is free software: you can redistribute it and/or modify
-
-# it under the terms of the GNU Affero General Public License as
-
-# published by the Free Software Foundation, either version 3 of the
-
-# License, or (at your option) any later version.
-
-#
-
-# breast_cancer_classifier is distributed in the hope that it will be useful,
-
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-
-# GNU Affero General Public License for more details.
-
-#
-
-# You should have received a copy of the GNU Affero General Public License
-
-# along with breast_cancer_classifier.  If not, see <http://www.gnu.org/licenses/>.
-
-# ==============================================================================
-
 """
-
 Runs the image only model and image+heatmaps model for breast cancer prediction.
-
 """
-
 import argparse
-
 import collections as col
-
 import numpy as np
-
 import os
-
 import pandas as pd
-
 import torch
-
 import tqdm
 
-
-
 import src.utilities.pickling as pickling
-
 import src.utilities.tools as tools
-
 import src.modeling.models as models
-
 import src.data_loading.loading as loading
-
 from src.constants import VIEWS, VIEWANGLES, LABELS
-
-
-
-
 
 def run_model(model, exam_list, parameters):
     """
