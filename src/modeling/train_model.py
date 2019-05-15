3393d2f83d14f256794b400389195760f14576d0
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2019-05-15 18:50:36 +0100

    Update train_model.py

--- src/modeling/train_model.py
+++ src/modeling/train_model.py
@@ -15,21 +15,20 @@ import src.modeling.models as models
 import src.data_loading.loading as loading
 from src.constants import VIEWS, VIEWANGLES, LABELS
 
-def train_model(model, exam_list, parameters):
-    input_channels = 3 if parameters["use_heatmaps"] else 1
-    model = models.SplitBreastModel(input_channels)
-    model.load_state_dict(torch.load(model_path)["model"])
-    exam_list = pickling.unpickle_from_file(data_path)
+#model, criterion, optimizer, scheduler, num_epochs=25
+def train_run_model(model, exam_list, parameters):
+    
     if (parameters["device_type"] == "gpu") and torch.has_cudnn:
         device = torch.device("cuda:{}".format(parameters["gpu_number"]))
     else:
         device = torch.device("cpu")
     model = model.to(device)
-    model.train()
+    model.eval()
     random_number_generator = np.random.RandomState(parameters["seed"])
     image_extension = ".hdf5" if parameters["use_hdf5"] else ".png"
     
     with torch.no_grad():
+        
         for datum in tqdm.tqdm(exam_list):
             predictions_for_datum = []
             loaded_image_dict = {view: [] for view in VIEWS.LIST}
@@ -40,11 +39,21 @@ def train_model(model, exam_list, parameters):
                         image_path=os.path.join(parameters["image_path"], short_file_path + image_extension),
                         view=view,
                         horizontal_flip=datum["horizontal_flip"],
-                    )    
-                    loaded_heatmaps = None
+                    )
+                    if parameters["use_heatmaps"]:
+                        loaded_heatmaps = loading.load_heatmaps(
+                            benign_heatmap_path=os.path.join(parameters["heatmaps_path"], "heatmap_benign",
+                                                             short_file_path + ".hdf5"),
+                            malignant_heatmap_path=os.path.join(parameters["heatmaps_path"], "heatmap_malignant",
+                                                                short_file_path + ".hdf5"),
+                            view=view,
+                            horizontal_flip=datum["horizontal_flip"],
+                        )
+                    else:
+                        loaded_heatmaps = None
                     loaded_image_dict[view].append(loaded_image)
                     loaded_heatmaps_dict[view].append(loaded_heatmaps)
-            for data_batch in tools.partition_batch(range(parameters["num_epochs"]), parameters["batch_size"]):
+            for data_batch in tools.partition_batch(1, 1):
                 batch_dict = {view: [] for view in VIEWS.LIST}
                 for _ in data_batch:
                     for view in VIEWS.LIST:
@@ -61,20 +70,96 @@ def train_model(model, exam_list, parameters):
                             max_crop_noise=parameters["max_crop_noise"],
                             max_crop_size_noise=parameters["max_crop_size_noise"],
                         )
-                        batch_dict[view].append(cropped_image[:, :, np.newaxis])
+                        if loaded_heatmaps_dict[view][image_index] is None:
+                            batch_dict[view].append(cropped_image[:, :, np.newaxis])
+                        else:
+                            batch_dict[view].append(np.concatenate([
+                                cropped_image[:, :, np.newaxis],
+                                cropped_heatmaps,
+                            ], axis=2))
+
                 tensor_batch = {
                     view: torch.tensor(np.stack(batch_dict[view])).permute(0, 3, 1, 2).to(device)
                     for view in VIEWS.LIST
                 }
+                
                 output = model(tensor_batch)
                 batch_predictions = compute_batch_predictions(output)
                 pred_df = pd.DataFrame({k: v[:, 1] for k, v in batch_predictions.items()})
                 pred_df.columns.names = ["label", "view_angle"]
                 predictions = pred_df.T.reset_index().groupby("label").mean().T.values
-                predictions_for_datum.append(predictions)
-           predictions_ls.append(np.mean(np.concatenate(predictions_for_datum, axis=0), axis=0))
-
                 
+            
+    
+         
+    
+ 
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model               
 
 def compute_batch_predictions(y_hat):
     """
@@ -90,7 +175,7 @@ def compute_batch_predictions(y_hat):
     return batch_prediction_dict
 
 
-def load_train_save(model_path, data_path, output_path, parameters):
+def load_train_save(model_path, data_path, output_path, parameters, truth_values):
     """
     Outputs the predictions as csv file
     """
@@ -98,9 +183,8 @@ def load_train_save(model_path, data_path, output_path, parameters):
     model = models.SplitBreastModel(input_channels)
     model.load_state_dict(torch.load(model_path)["model"])
     exam_list = pickling.unpickle_from_file(data_path)
-    predictions = run_model(model, exam_list, parameters)
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    # Take the positive prediction
+    train_run_model( model, exam_list, parameters, truth_values, model_path)
+   
     
     
 
@@ -108,6 +192,7 @@ def main():
     parser = argparse.ArgumentParser(description='Run image-only model or image+heatmap model')
     parser.add_argument('--model-path', required=True)
     parser.add_argument('--data-path', required=True)
+    parser.add_argument('--truthvalues-path', required=True)
     parser.add_argument('--image-path', required=True)
     parser.add_argument('--output-path', required=True)
     parser.add_argument('--batch-size', default=1, type=int)
@@ -141,6 +226,7 @@ def main():
         data_path=args.data_path,
         output_path=args.output_path,
         parameters=parameters,
+        truth_values=args.truthvalues_path,
     )
  
 
